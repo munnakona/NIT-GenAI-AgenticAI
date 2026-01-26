0b0b34d910af1562e0b44a82d43a68b1b846ddf0
Munna <munna033@gmail.com>
2026-01-26 16:22:43 +0300

    Introduced Regularization and Polynomial Regression models

---
+++ Polynomial_Technique/Polynomial.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 26 15:32:37 2026
+
+@author: munna
+"""
+
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r"/Users/munna/VScode/NareshIT/Polynomial_Technique/emp_sal.csv")
+
+
+X = dataset.iloc[:,1:2].values
+
+y = dataset.iloc[:,2].values
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X),color= 'blue')
+plt.title('Linear Regression Graph')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+lin_model_pred = lin_reg.predict([[6.5]])
+lin_model_pred
+
+
+from sklearn.preprocessing import PolynomialFeatures
+
+poly_reg = PolynomialFeatures()
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly,y)
+
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)),color= 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+# Prediction
+
+poly_model_pred = lin_reg_2.predict(poly_reg.fit_transform(X))
+
+lin_model_pred = lin_reg.predict([[6.5]])
+lin_model_pred
+
+poly_model_pred = lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+poly_model_pred
+
+
+
+# SVR Model
+
+from sklearn.svm import SVR
+
+svr_reg = SVR(kernel='poly',degree=5)
+svr_reg.fit(X, y)
+
+svr_model_pred = svr_reg.predict([[6.5]])
+svr_model_pred
+
+
+
+
+
+
+
+
+
---
+++ Regularization_Technique/Regulization_tech.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 24 15:56:41 2026
+
+@author: munna
+"""
+
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv(r"/Users/munna/VScode/NareshIT/Regularization_Technique/car-mpg.csv")
+
